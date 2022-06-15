95eb6860d14355699fb7d66b341143fe26a5ecf2
tanyalev06 <grib.tanya06@mail.ru>
2022-06-15 22:26:15 +0300

    feat: adaptive is done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//самовызывывающаяся функция
 (function () {
     const header = document.querySelector('.header');
     //при скроллинге страницы будет выполняться функция 
@@ -9,4 +10,19 @@
             header.classList.remove('header_active');
         }
     }
+}());
+
+//burger handler
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    // console.log(burgerItem);
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    burgerItem.addEventListener('click', () => {
+        // console.log(1);
+        menu.classList.add('header__nav_active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__nav_active');
+    })
 }());

418301193a37274183ef22609967c2864a17a206
tanyalev06 <grib.tanya06@mail.ru>
2022-06-16 08:32:42 +0300

    feat: smooth scroll is done

--- js/main.js
+++ js/main.js
@@ -26,3 +26,43 @@
         menu.classList.remove('header__nav_active');
     })
 }());
+
+//Scroll to anchors
+(function () {
+    const SmoothScroll = function (targetEI, duration) {
+        const HeaderEIHeight = document.querySelector('.header').clientHeight;
+        let target = document.querySelector(targetEI);
+        let targetPosition = target.getBoundingClientRect().top - HeaderEIHeight;//getBoundingClientRect() - возвращает размер элемента и его позицию относительно viewport (часть страницы, показанная на экране, и которую мы видим).
+        let startPosition = window.pageYOffset;//возвращает количество пикселей, на которое прокручен документ по вертикали (вниз или вверх)
+        let startTime = null;
+
+        //функция-обработчик скролла - функция IS
+        const ease = function(t,b,c,d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+
+        //функция анимации
+        const animation = function(currentTime) {
+            if (startTime === null) startTime = currentTime;
+            const timeELapsed = currentTime - startTime;
+            const run = ease(timeELapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0, run);
+            if (timeELapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+    };
+    //функция подвешивания обработчика события на ссылки
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                SmoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
+}());
